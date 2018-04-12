996124fc4b1a282bb8e2c485a31b0396f1902b62
MarcosLaydner <30905990+MarcosLaydner@users.noreply.github.com>
2018-04-12 15:29:07 -0300

    search and insert complete

--- Trabalhinho_1/src/trabalhinho1/OrderedList.java
+++ Trabalhinho_1/src/trabalhinho1/OrderedList.java
@@ -12,7 +12,46 @@ public class OrderedList {
 		this.last = null;
 	}
 
-	public boolean search(Person pers) {
+// Busca e exluir devem ter como parametro o id, segundo o arquivo do moodle
+// então não me questiona sobre isso	
+	
+	
+	public Person search(int id ) {
+		
+		Box current = first;
+		Person found = null;
+		
+		if (first != null) {
+			
+			for (int i = 0; i >= count; i++) {
+				if (current.getData().getId() == id) {
+					found = current.getData();
+				} else {
+					current = current.getNext();
+				}
+			}
+		} 
+		return found;
+	}
+
+	public void OrderdInsert(Person person) {
+		
+		Box current = first;
+		
+		if (first != null) {
+			
+			while (current.getData().getId() < person.getId()) {
+				current = current.getNext();
+			}
+			
+			if (current.getData().getId() == person.getId()) {
+				//exception
+			} else {
+				Box insert = new Box(current.getPrevious(), person, current);
+				current.getPrevious().setNext(insert);
+				current.setPrevious(insert);
+			}
+		}
 		
 	}
 
