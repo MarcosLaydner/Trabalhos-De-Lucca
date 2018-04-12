3f20d7e0198c5de727cd8b5a6cbb2fbdb1094e3e
MarcosLaydner <30905990+MarcosLaydner@users.noreply.github.com>
2018-04-12 14:19:39 -0300

    lista

--- Trabalhinho_1/src/trabalhinho1/OrderedList.java
+++ Trabalhinho_1/src/trabalhinho1/OrderedList.java
@@ -1,5 +1,19 @@
 package trabalhinho1;
 
 public class OrderedList {
+	
+	public int count;
+	public Box first;
+	public Box last;
+	
+	public OrderedList() {
+		this.count = 0;
+		this.first = null;
+		this.last = null;
+	}
+
+	public boolean search(Person pers) {
+		
+	}
 
 }
