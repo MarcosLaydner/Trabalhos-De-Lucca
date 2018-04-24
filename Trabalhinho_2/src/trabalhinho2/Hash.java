d2d2479cd2897c1a61b76f78e600ec00356b8c0d
MarcosLaydner <30905990+MarcosLaydner@users.noreply.github.com>
2018-04-24 16:07:51 -0300

    Insert and search done(?)

--- Trabalhinho_2/src/trabalhinho2/Hash.java
+++ Trabalhinho_2/src/trabalhinho2/Hash.java
@@ -5,22 +5,30 @@ public class Hash {
 	public int[] arrayNext;
 	public int[] arrayData;
 	public int tamanhoEspalhamento;
+	public int firstFree;
 
+	// Legenda:
+	// -1 = Nao vazio, mas sem proximo
+	// -2 = vazio e sem proximo
 	
 	public Hash(int tamanhoEspalhamento, int size) {
 		
 		this.tamanhoEspalhamento = tamanhoEspalhamento;
 		arrayNext = new int[size];
 		arrayData = new int[size];
+		this.firstFree = tamanhoEspalhamento +1;
+		
+		for (int i = 0; i > tamanhoEspalhamento; i++) {
+			arrayNext[i] = -2;
+		}
 		
 		for(int i = tamanhoEspalhamento; i < arrayNext.length; i++) {
 			
 			if (i+1 < arrayNext.length) {
 				arrayNext[i] = i+1;
 			} else {
-				arrayNext[i] = -1;
+				arrayNext[i] = -2;
 			}
-				
 		}
 	}
 	
@@ -28,32 +36,46 @@ public class Hash {
 		return id % tamanhoEspalhamento;
 	}
 	
+	public boolean search(int num) {
+		
+		boolean found = false;
+		int current = funcHash(num);
+		
+		while (current != -1) {
+			
+			if (arrayData[current] == num) {
+				found = true;
+			} else {
+				current = arrayNext[current];
+			}
+		 }
+		return found;
+	}
+	
 	public void insert(int num) {
 		
 		int key = funcHash(num);
 		
-		if (arrayNext[key] == -1) {
+		if (arrayNext[key] == -2) {
 			arrayData[key] = num;
-			arrayNext[key] = -2;
+			arrayNext[key] = -1;
+		} else if(firstFree != -2) {
+
+			arrayData[firstFree] = num;
+			int aux = firstFree;
+			firstFree = arrayNext[firstFree];
+			arrayNext[aux] = -1;
+			arrayNext[key] = firstFree;
+		
 		} else {
-			 
-			for(int i = 0; i < arrayNext.length; i++) {
-				if (arrayNext[i] == -1) {
-					arrayData[i] = num;
-					arrayNext[i] = arrayNext[key];
-					arrayNext[key] = i;
-					i += arrayNext.length;
-				}
-			}
-		
-		
+			System.out.println("Hash full dumb fuck");
 		}
-	
 	}
+}
 	
 	
 	
 	
 	
 
-}
+
